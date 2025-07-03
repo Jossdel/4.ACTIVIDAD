7f66d097741a0ad735d5553ded17209d01bd8dee
Jossdel <josuedelgadil01@gmail.com>
2025-07-02 21:06:32 -0400

    Ejercicio 1

--- 4.ACTIVIDAD/4.ACTIVIDAD/RegistrosDeUsuarios.Designer.cs
+++ 4.ACTIVIDAD/4.ACTIVIDAD/RegistrosDeUsuarios.Designer.cs
@@ -28,20 +28,318 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtOcupacion = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtEstadoCivil = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtCiudad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dirección";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 29);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Teléfono";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 357);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 29);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ciudad";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(23, 411);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 29);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "País";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 460);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 29);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Cédula";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 510);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 29);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Género";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(28, 564);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 29);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Ocupación";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(23, 610);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(140, 29);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Estado Civil";
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(168, 405);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(491, 35);
+            this.txtPais.TabIndex = 12;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(169, 297);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(491, 35);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(169, 244);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(491, 35);
+            this.txtTelefono.TabIndex = 14;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(169, 457);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(491, 35);
+            this.txtCedula.TabIndex = 15;
+            // 
+            // txtOcupacion
+            // 
+            this.txtOcupacion.Location = new System.Drawing.Point(169, 564);
+            this.txtOcupacion.Name = "txtOcupacion";
+            this.txtOcupacion.Size = new System.Drawing.Size(491, 35);
+            this.txtOcupacion.TabIndex = 16;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(169, 510);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(491, 35);
+            this.txtGenero.TabIndex = 17;
+            this.txtGenero.TextChanged += new System.EventHandler(this.txtGenero_TextChanged);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(167, 136);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(491, 35);
+            this.txtEdad.TabIndex = 18;
+            // 
+            // txtEstadoCivil
+            // 
+            this.txtEstadoCivil.Location = new System.Drawing.Point(169, 610);
+            this.txtEstadoCivil.Name = "txtEstadoCivil";
+            this.txtEstadoCivil.Size = new System.Drawing.Size(492, 35);
+            this.txtEstadoCivil.TabIndex = 19;
+            this.txtEstadoCivil.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(167, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(491, 35);
+            this.txtNombre.TabIndex = 20;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(167, 81);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(491, 35);
+            this.txtApellido.TabIndex = 21;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(167, 189);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(491, 35);
+            this.txtDireccion.TabIndex = 22;
+            // 
+            // txtCiudad
+            // 
+            this.txtCiudad.Location = new System.Drawing.Point(169, 351);
+            this.txtCiudad.Name = "txtCiudad";
+            this.txtCiudad.Size = new System.Drawing.Size(491, 35);
+            this.txtCiudad.TabIndex = 23;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(167, 662);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(494, 61);
+            this.btnAgregar.TabIndex = 24;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 729);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 92;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(2690, 476);
+            this.dataGridView1.TabIndex = 25;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // RegistrosDeUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2714, 1227);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCiudad);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtEstadoCivil);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtOcupacion);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "RegistrosDeUsuarios";
             this.Text = "RegistrosDeUsuarios";
             this.Load += new System.EventHandler(this.RegistrosDeUsuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtOcupacion;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtEstadoCivil;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtCiudad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- 4.ACTIVIDAD/4.ACTIVIDAD/RegistrosDeUsuarios.cs
+++ 4.ACTIVIDAD/4.ACTIVIDAD/RegistrosDeUsuarios.cs
@@ -19,6 +19,71 @@ namespace _4.ACTIVIDAD
 
         private void RegistrosDeUsuarios_Load(object sender, EventArgs e)
         {
+            dataGridView1.ColumnCount = 12;
+            dataGridView1.Columns[0].Name = "Nombre";
+            dataGridView1.Columns[1].Name = "Apellido";
+            dataGridView1.Columns[2].Name = "Edad";
+            dataGridView1.Columns[3].Name = "Dirección";
+            dataGridView1.Columns[4].Name = "Teléfono";
+            dataGridView1.Columns[5].Name = "Email";
+            dataGridView1.Columns[6].Name = "Ciudad";
+            dataGridView1.Columns[7].Name = "País";
+            dataGridView1.Columns[8].Name = "Cédula";
+            dataGridView1.Columns[9].Name = "Género";
+            dataGridView1.Columns[10].Name = "Ocupación";
+            dataGridView1.Columns[11].Name = "Estado Civil";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Validación básica (opcional)
+            if (txtNombre.Text == "" || txtApellido.Text == "")
+            {
+                MessageBox.Show("Por favor, complete al menos nombre y apellido.");
+                return;
+            }
+
+            // Agrega los datos al DataGridView
+            dataGridView1.Rows.Add(
+                txtNombre.Text,
+                txtApellido.Text,
+                txtEdad.Text,
+                txtDireccion.Text,
+                txtTelefono.Text,
+                txtEmail.Text,
+                txtCiudad.Text,
+                txtPais.Text,
+                txtCedula.Text,
+                txtGenero.Text,
+                txtOcupacion.Text,
+                txtEstadoCivil.Text
+            );
+
+            // Limpia los campos después de agregar
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                    ((TextBox)ctrl).Clear();
+            }
+        }
+
+        private void txtGenero_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
